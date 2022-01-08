2b79cf324b6494530700bd39dee0b2af74dd3387
galaxygoldfish <shriscu6340@gmail.com>
2022-01-07 20:38:29 -0800

    [Feature] [iOS] Reformat assets into colors and icons, apply saved color to notes in list

--- ios/Compose/Views/NoteListView.swift
+++ ios/Compose/Views/NoteListView.swift
@@ -27,26 +27,45 @@ struct NoteListView: View {
                 Spacer()
             }
             .frame(width: UIScreen.main.bounds.width * 0.94)
-            .padding(.top, 10)
+            .padding(.top, 5)
         }
+        .animation(.easeInOut)
     }
 }
 
 struct NoteListCard: View {
+    
+    @Environment(\.colorScheme) private var colorScheme
     var item: NoteDocument
+
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 1) {
             Text(item.title)
+                .font(typographyH6)
+                .padding(.horizontal, 10)
+                .padding(.top, 10)
+                .foregroundColor(.black)
             Text(item.content)
                 .lineLimit(8)
+                .font(typographyBody1)
+                .padding(.horizontal, 10)
+                .padding(.top, 2)
+                .foregroundColor(.black)
             HStack {
                 Text(item.date)
+                    .font(typographyBody1)
+                Spacer()
                 Text(item.time)
+                    .font(typographyBody1)
             }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 5)
+            .padding(.bottom, 5)
+            .foregroundColor(colorScheme == .dark ? Color("NeutralGray") : .black.opacity(0.8))
         }
-        .frame(width: UIScreen.main.bounds.width * 0.435)
-        .background(Color("NeutralGrayDark"))
+        .frame(width: UIScreen.main.bounds.width * 0.445)
+        .background(Color("CardColor\(item.color)"))
         .cornerRadius(10)
-        .padding(.bottom, 5)
+        .padding(.bottom, 2)
     }
 }
